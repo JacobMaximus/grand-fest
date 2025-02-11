d3aea43f627dea25bfad3e7c304f1b0aef5e6af0
JacobMaximus <178012512+JacobMaximus@users.noreply.github.com>
2025-02-12 01:09:15 +0530

    Finished adding wavy background to gallery.

--- js/events.js
+++ js/events.js
@@ -50,3 +50,7 @@ function openModal(event) {
 eventDivs.forEach(div => {
     div.addEventListener('click', openModal);
 });
+
+document.querySelectorAll("img.random-img").forEach(img => {
+    img.src = `https://picsum.photos/270/250?random=${Math.random()}`;
+});
